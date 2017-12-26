f80d3bed4d06861c275600990982f4bec4c18de8
Rich Sharp <richpsharp@gmail.com>
2017-12-25 18:09:53 -0800

    using dill for dem id path map and using tree serialization for rtree dem

--- ipbes-ndr-analysis.py
+++ ipbes-ndr-analysis.py
@@ -1,7 +1,7 @@
 """Script to manage NDR runs for IPBES project."""
 import logging
 import os
-import pickle
+import dill
 
 import taskgraph
 import rtree.index
@@ -38,7 +38,7 @@ WATERSHED_PATH_LIST = glob.glob(
 TARGET_WORKSPACE = 'ndr_workspace'
 TASKGRAPH_DIR = os.path.join(TARGET_WORKSPACE, 'taskgraph_cache')
 
-RTREE_PATH = 'dem_rtree.dat'
+RTREE_PATH = 'dem_rtree'
 
 
 def main():
@@ -60,10 +60,30 @@ def main():
         'dem_globe_CGIAR_STRMv41_3arcseconds', '*.tif'))
 
     dem_path_index_map = {}
+    dem_path_index_map_path = os.path.join(
+        TARGET_WORKSPACE, 'dem_path_index_map.dat')
     build_dem_rtree_task = task_graph.add_task(
         func=build_dem_rtree,
-        args=(dem_path_list, dem_path_index_map, dem_rtree_path),
-        target_path_list=[dem_rtree_path])
+        args=(dem_path_list, dem_path_index_map_path, dem_rtree_path),
+        target_path_list=[
+            dem_rtree_path+'.dat',  # rtree adds a ".dat" file
+            dem_path_index_map_path],
+        task_name='build_dem_rtree')
+
+    task_graph.close()
+    task_graph.join()
+
+    watershed_path = r"C:\Users\Rich\Dropbox\ipbes-data\watersheds_globe_HydroSHEDS_15arcseconds\af_bas_15s_beta.shp"
+    watershed_id = 98949
+    watershed_basename = os.path.splitext(os.path.basename(watershed_path))[0]
+    target_dem_path = os.path.join(
+        TARGET_WORKSPACE,  "ws_%s_%d_working_dir" % (
+            watershed_basename, watershed_id), 'ws_%d_dem.tif' % watershed_id)
+    merge_watershed_dems(
+        watershed_path, watershed_id, dem_rtree_path, dem_path_index_map_path,
+        target_dem_path)
+
+    return
 
     for watershed_path in WATERSHED_PATH_LIST:
         watershed_vector = ogr.Open(watershed_path)
@@ -88,7 +108,7 @@ def main():
 
 
 def merge_watershed_dems(
-        watershed_path, watershed_id, dem_rtree_path, dem_path_index_map,
+        watershed_path, watershed_id, dem_rtree_path, dem_path_index_map_path,
         target_dem_path):
     """Find DEMs that overlap the given watershed polyon by id.
 
@@ -97,8 +117,8 @@ def merge_watershed_dems(
         watershed_id (int): feature number to index and overlap.
         dem_rtree_path (string): path to a pickled rtree that maps
             bounding boxes to dem ids.
-        dem_path_index_map (dict): maps DEM ids from the rtree to filepaths.
-        dem_path_id_map (dict): map dem id to dem paths.
+        dem_path_index_map_path (string): path to a pickled map to maps DEM
+            ids from the rtree to filepaths.
         target_dem_path (string): path to file that's created by
             mosaicing all the overlapping dems together, suitable for
             routing in the given watershed.
@@ -116,37 +136,42 @@ def merge_watershed_dems(
     watershed_feature = None
     watershed_vector = None
 
-    with open(dem_rtree_path, 'rb') as dem_rtree_file:
-        dem_rtree = pickle.load(dem_rtree_file)
+    LOGGER.debug(watershed_bb)
+    dem_rtree = rtree.index.Index(dem_rtree_path)
+    LOGGER.debug(dem_rtree.bounds)
+
+    with open(dem_path_index_map_path, 'rb') as f:
+        dem_path_index_map = dill.load(f)
 
     overlapping_dem_list = list(dem_rtree.intersection(watershed_bb))
 
     if len(overlapping_dem_list) > 0:
-        LOGGER.debug(
-            watershed_id, [
-                dem_path_index_map[i] for i in overlapping_dem_list])
+        overlapping_dem_path_list = [
+            dem_path_index_map[i] for i in overlapping_dem_list]
+        LOGGER.debug("%s %s", watershed_id, overlapping_dem_path_list)
         workspace_dir = os.path.dirname(target_dem_path)
         if not os.path.exists(workspace_dir):
             os.makedirs(workspace_dir)
-        pygeoprocessing.merge_rasters(overlapping_dem_list, target_dem_path)
+        pygeoprocessing.merge_rasters(
+            overlapping_dem_path_list, target_dem_path)
     else:
         LOGGER.debug(
             "no overlapping dems found for %s wsid %d", watershed_path,
             watershed_id)
 
 
-
-def build_dem_rtree(dem_path_list, dem_path_index_map, dem_rtree_path):
+def build_dem_rtree(dem_path_list, dem_path_index_map_path, dem_rtree_path):
     """Build RTree indexed by FID for points in `wwwiii_vector_path`."""
-    dem_rtree = rtree.index.Index()
+    dem_rtree = rtree.index.Index(dem_rtree_path)
+    dem_path_index_map = {}
     for dem_id, dem_path in enumerate(dem_path_list):
         raster_info = pygeoprocessing.get_raster_info(dem_path)
         dem_path_index_map[dem_id] = dem_path
-        print os.path.basename(dem_path), raster_info['bounding_box']
+        LOGGER.debug("inserting %s %s", raster_info['bounding_box'], os.path.basename(dem_path))
         dem_rtree.insert(dem_id, raster_info['bounding_box'])
-    with open(dem_rtree_path, 'wb') as dem_rtree_file:
-        pickle.dump(dem_rtree, dem_rtree_file)
-
+    dem_rtree.close()
+    with open(dem_path_index_map_path, 'wb') as f:
+        dill.dump(dem_path_index_map, f)
 
 if __name__ == '__main__':
     main()

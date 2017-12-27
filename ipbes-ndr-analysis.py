9fbfb173236c5df9de287a8db52cbf9a108651ee
Rich Sharp <richpsharp@gmail.com>
2017-12-27 01:15:23 -0800

    added reprojection, calculation of s_accum

--- ipbes-ndr-analysis.py
+++ ipbes-ndr-analysis.py
@@ -2,6 +2,7 @@
 import logging
 import os
 import glob
+import math
 
 import taskgraph
 import numpy
@@ -10,10 +11,11 @@ import dill
 import rtree.index
 from osgeo import ogr
 from osgeo import gdal
+from osgeo import osr
 import pygeoprocessing
 import pygeoprocessing.routing
 
-N_CPUS = 1
+N_CPUS = -1
 NODATA = -1
 
 logging.basicConfig(
@@ -46,6 +48,24 @@ TASKGRAPH_DIR = os.path.join(TARGET_WORKSPACE, 'taskgraph_cache')
 
 RTREE_PATH = 'dem_rtree'
 
+def length_of_degree(lat, lng):
+    """Calcualte the length of a degree in meters."""
+    m1 = 111132.92
+    m2 = -559.82
+    m3 = 1.175
+    m4 = -0.0023
+    p1 = 111412.84
+    p2 = -93.5
+    p3 = 0.118
+
+    lat = -19.7860856226 * math.pi / 180
+
+    latlen = (
+        m1 + (m2 * math.cos(2 * lat)) + (m3 * math.cos(4 * lat)) + (m4 * math.cos(6 * lat)))
+    longlen = abs(
+        (p1 * math.cos(lat)) + (p2 * math.cos(3 * lat)) + (p3 * math.cos(5 * lat)))
+
+    return max(latlen, longlen)
 
 def mult_arrays(*array_list):
     """Multiply arrays in array list but block out stacks with NODATA."""
@@ -105,13 +125,16 @@ def main():
         task_name='build_dem_rtree')
 
     watershed_path = r"C:\Users\Rich\Dropbox\ipbes-data\watersheds_globe_HydroSHEDS_15arcseconds\af_bas_15s_beta.shp"
+    watershed_vector = ogr.Open(watershed_path)
+    watershed_layer = watershed_vector.GetLayer()
+
     watershed_id = 85668
     watershed_basename = os.path.splitext(os.path.basename(watershed_path))[0]
+    ws_prefix = 'ws_%s_%d' % (watershed_basename, watershed_id)
     ws_working_dir = os.path.join(
-        TARGET_WORKSPACE, "ws_%s_%d_working_dir" % (
-            watershed_basename, watershed_id))
+        TARGET_WORKSPACE, "%s_working_dir" % ws_prefix)
     watershed_dem_path = os.path.join(
-        ws_working_dir, 'ws_%d_dem.tif' % watershed_id)
+        ws_working_dir, 'ws_%s_dem.tif' % ws_prefix)
     merge_watershed_dems_task = task_graph.add_task(
         func=merge_watershed_dems,
         args=(
@@ -119,7 +142,23 @@ def main():
             dem_path_index_map_path, watershed_dem_path),
         target_path_list=[watershed_dem_path],
         dependent_task_list=[build_dem_rtree_task],
-        task_name='merge_watershed_dems_%d' % watershed_id)
+        task_name='merge_watershed_dems_%s' % ws_prefix)
+
+    watershed_feature = watershed_layer.GetFeature(watershed_id)
+    feature_geom = watershed_feature.GetGeometryRef()
+    feature_centroid = feature_geom.Centroid()
+
+    utm_code = (math.floor((feature_centroid.GetX() + 180)/6) % 60) + 1
+    lat_code = 6 if feature_centroid.GetY() > 0 else 7
+    epsg_code = int('32%d%02d' % (lat_code, utm_code))
+    epsg_srs = osr.SpatialReference()
+    epsg_srs.ImportFromEPSG(epsg_code)
+    utm_pixel_size = abs(dem_pixel_size[0]) * length_of_degree(
+        feature_centroid.GetY(), feature_centroid.GetX())
+
+    feature_centroid = None
+    feature_geom = None
+    watershed_feature = None
 
     # clip precip raster to watershed bb
     precip_raster_path = os.path.join(
@@ -136,8 +175,8 @@ def main():
 
     aligned_path_list = [
         os.path.join(
-            ws_working_dir, os.path.splitext(
-                os.path.basename(x))[0]+'_aligned.tif')
+            ws_working_dir, '%s_%s_aligned.tif' % (
+                ws_prefix, os.path.splitext(os.path.basename(x))[0]))
         for x in base_raster_path_list]
 
     # clip dem, precip, & landcover to size of DEM? use 'mode'
@@ -148,27 +187,49 @@ def main():
             ['nearest', 'nearest', 'mode'], dem_pixel_size, 'intersection'),
         target_path_list=aligned_path_list,
         dependent_task_list=[merge_watershed_dems_task],
-        task_name='aign_resize_task_%d' % watershed_id)
+        task_name='align_resize_task_%s' % ws_prefix)
 
-    # fill and route dem, aligned_path_list[0] - dem
-    filled_watershed_dem_path = '%s_filled.tif' % (
-        os.path.splitext(aligned_path_list[0])[0])
-    flow_dir_watershed_dem_path = '%s_flow_dir.tif' % (
-        os.path.splitext(aligned_path_list[0])[0])
+    utm_dem_path = os.path.join(
+        ws_working_dir, '%s_%s_dem.tif' % (ws_prefix, epsg_code))
+    utm_precip_path = os.path.join(
+        ws_working_dir, '%s_%s_precip.tif' % (ws_prefix, epsg_code))
+    utm_landcover_path = os.path.join(
+        ws_working_dir, '%s_%s_landcover.tif' % (ws_prefix, epsg_code))
+
+    path_task_id_map = {}
+    for raster_id, base_path, target_path in [
+            ('dem', aligned_path_list[0], utm_dem_path),
+            ('precip', aligned_path_list[1], utm_precip_path),
+            ('landcover', aligned_path_list[2], utm_landcover_path)]:
+        # determine target pixel size by determining length of degree
+        task = task_graph.add_task(
+            func=pygeoprocessing.warp_raster,
+            args=(
+                base_path, (utm_pixel_size, -utm_pixel_size), target_path,
+                'nearest'),
+            kwargs={'target_sr_wkt': epsg_srs.ExportToWkt()},
+            target_path_list=[target_path],
+            dependent_task_list=[align_resize_task],
+            task_name='warp_raster_%s' % raster_id)
+        path_task_id_map[raster_id] = (target_path, task)
+
+    # fill and route dem
+    filled_watershed_dem_path = '%s_filled.tif' % ws_prefix
+    flow_dir_watershed_dem_path = '%s_flow_dir.tif' % ws_prefix
     fill_pits_task = task_graph.add_task(
         func=pygeoprocessing.routing.fill_pits,
         args=(
-            (aligned_path_list[0], 1), filled_watershed_dem_path,
+            (path_task_id_map['dem'][0], 1), filled_watershed_dem_path,
             flow_dir_watershed_dem_path),
         kwargs={'temp_dir_path': ws_working_dir},
         target_path_list=[
             filled_watershed_dem_path, flow_dir_watershed_dem_path],
-        dependent_task_list=[align_resize_task],
-        task_name='fill_pits_task_%d' % watershed_id)
+        dependent_task_list=[path_task_id_map['dem'][1]],
+        task_name='fill_pits_task_%s' % ws_prefix)
 
     # flow accum dem
-    flow_accum_watershed_dem_path = '%s_flow_accum.tif' % (
-        os.path.splitext(watershed_dem_path)[0])
+    flow_accum_watershed_dem_path = os.path.join(
+        ws_working_dir, '%s_flow_accum.tif' % ws_prefix)
     flow_accmulation_task = task_graph.add_task(
         func=pygeoprocessing.routing.flow_accmulation,
         args=(
@@ -176,59 +237,72 @@ def main():
         kwargs={'temp_dir_path': ws_working_dir},
         target_path_list=[flow_accum_watershed_dem_path],
         dependent_task_list=[fill_pits_task],
-        task_name='flow_accmulation_%d' % watershed_id)
+        task_name='flow_accmulation_%s' % ws_prefix)
 
     # reclassify eff_n
     eff_n_lucode_map = dict(
         zip(biophysical_table['ID'], biophysical_table['eff_n']))
-    # aligned[2] - landcover
-    eff_n_raster_path = os.path.join(ws_working_dir, 'eff_n.tif')
+    eff_n_raster_path = os.path.join(
+        ws_working_dir, '%s_eff_n.tif' % ws_prefix)
     reclassify_eff_n_task = task_graph.add_task(
         func=pygeoprocessing.reclassify_raster,
         args=(
-            (aligned_path_list[2], 1), eff_n_lucode_map, eff_n_raster_path,
-            gdal.GDT_Float32, NODATA),
+            (path_task_id_map['landcover'][0], 1), eff_n_lucode_map,
+            eff_n_raster_path, gdal.GDT_Float32, NODATA),
         target_path_list=[eff_n_raster_path],
-        dependent_task_list=[align_resize_task],
-        task_name='reclasify_eff_n_%d' % watershed_id)
+        dependent_task_list=[path_task_id_map['landcover'][1]],
+        task_name='reclasify_eff_n_%s' % ws_prefix)
 
     # reclassify load_n
-    load_n_raster_path = os.path.join(ws_working_dir, 'load_n.tif')
+    load_n_raster_path = os.path.join(
+        ws_working_dir, '%s_load_n.tif' % ws_prefix)
     load_n_lucode_map = dict(
         zip(biophysical_table['ID'], biophysical_table['load_n']))
     reclassify_load_n_task = task_graph.add_task(
         func=pygeoprocessing.reclassify_raster,
         args=(
-            (aligned_path_list[2], 1), load_n_lucode_map, load_n_raster_path,
-            gdal.GDT_Float32, NODATA),
+            (path_task_id_map['landcover'][0], 1), load_n_lucode_map,
+            load_n_raster_path, gdal.GDT_Float32, NODATA),
         target_path_list=[load_n_raster_path],
-        dependent_task_list=[align_resize_task],
-        task_name='reclasify_load_n_%d' % watershed_id)
+        dependent_task_list=[path_task_id_map['landcover'][1]],
+        task_name='reclasify_load_n_%s' % ws_prefix)
 
     # calculate modified load (load * precip)
     modified_load_raster_path = os.path.join(
-        ws_working_dir, 'modified_load.tif')
-    #aligned_path_list[1] - precipitation
+        ws_working_dir, '%s_modified_load.tif' % ws_prefix)
     modified_load_task = task_graph.add_task(
         func=pygeoprocessing.raster_calculator,
         args=(
-            [(load_n_raster_path, 1), (aligned_path_list[1], 1)],
+            [(load_n_raster_path, 1), (path_task_id_map['precip'][0], 1)],
             mult_arrays, modified_load_raster_path, gdal.GDT_Float32, NODATA),
         target_path_list=[modified_load_raster_path],
-        dependent_task_list=[reclassify_load_n_task, align_resize_task],
-        task_name='modified_load_%d' % watershed_id)
+        dependent_task_list=[
+            reclassify_load_n_task, path_task_id_map['precip'][1]],
+        task_name='modified_load_%s' % ws_prefix)
 
     # calculate slope
-    # aligned_path_list[0] is dem
-    target_slope_path = os.path.join(ws_working_dir, 'slope.tif')
+    target_slope_path = os.path.join(
+        ws_working_dir, '%s_slope.tif' % ws_prefix)
     calculate_slope_task = task_graph.add_task(
         func=pygeoprocessing.routing.calculate_slope,
-        args=((aligned_path_list[0], 1), target_slope_path),
+        args=((path_task_id_map['dem'][0], 1), target_slope_path),
         target_path_list=[target_slope_path],
-        dependent_task_list=[align_resize_task],
-        task_name='calculate_slope_%d' % watershed_id)
+        dependent_task_list=[path_task_id_map['dem'][1]],
+        task_name='calculate_slope_%s' % ws_prefix)
 
-    # calcualte D_up
+    # calculate D_up
+    slope_accum_watershed_dem_path = os.path.join(
+        ws_working_dir, '%s_s_accum.tif' % ws_prefix)
+    slope_accmulation_task = task_graph.add_task(
+        func=pygeoprocessing.routing.flow_accmulation,
+        args=(
+            (flow_dir_watershed_dem_path, 1), slope_accum_watershed_dem_path),
+        kwargs={
+            'temp_dir_path': ws_working_dir,
+            'weight_raster_band_path': (target_slope_path, 1)},
+        target_path_list=[slope_accum_watershed_dem_path],
+        dependent_task_list=[fill_pits_task, calculate_slope_task],
+        task_name='slope_accmulation_%s' % ws_prefix)
 
     # calculate flow path length down to stream
 

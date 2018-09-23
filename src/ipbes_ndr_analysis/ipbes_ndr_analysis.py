52de292256cc3613783f1fb09ac33e044ff5af14
Rich Sharp <richpsharp@gmail.com>
2018-09-22 21:15:33 -0700

    moving to module

--- src/ipbes_ndr_analysis/ipbes_ndr_analysis.py
+++ src/ipbes_ndr_analysis/ipbes_ndr_analysis.py
@@ -23,9 +23,6 @@ from osgeo import osr
 import pygeoprocessing
 import pygeoprocessing.routing
 
-# Compiles cython on runtime.
-import pyximport
-pyximport.install()
 import ipbes_ndr_analysis_cython
 
 N_CPUS = 4
@@ -1431,22 +1428,3 @@ def threshold_flow_accumulation(
     pygeoprocessing.raster_calculator(
         [(flow_accum_path, 1)], threshold_op, target_channel_path,
         gdal.GDT_Float32, channel_nodata)
-
-
-if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        LOGGER.error(
-            "usage: python %s iam_token_path workspace_dir", sys.argv[0])
-        sys.exit(-1)
-    raw_iam_token_path = sys.argv[1]
-    raw_workspace_dir = sys.argv[2]
-    if not os.path.isfile(raw_iam_token_path):
-        LOGGER.error(
-            '%s is not a file, should be an iam token', raw_workspace_dir)
-        sys.exit(-1)
-    if os.path.isfile(raw_workspace_dir):
-        LOGGER.error(
-            '%s is supposed to be the workspace directory but points to an '
-            'existing file' % raw_workspace_dir)
-        sys.exit(-1)
-    main(raw_iam_token_path, raw_workspace_dir)

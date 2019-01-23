5041464d2bfcda9631aa333e8d6988463c284954
Rich Sharp <richpsharp@gmail.com>
2019-01-23 14:25:37 -0800

    adding mosaic tasks

--- ndr_mosaicking.py
+++ ndr_mosaicking.py
@@ -5,6 +5,7 @@ import logging
 import os
 import multiprocessing
 
+import numpy
 from osgeo import gdal
 from osgeo import osr
 import pygeoprocessing
@@ -119,6 +120,7 @@ def main():
     LOGGER.info("found all the raster suffixes in %s", sample_dirpath)
 
     for raster_suffix in RASTER_SUFFIXES_TO_AGGREGATE:
+        previous_project_task_list = []
         for dirpath, filenames in leaf_directory_list:
             try:
                 base_raster_path = next(iter(
@@ -144,11 +146,77 @@ def main():
                     global_raster_task_path_map[raster_suffix][0]],
                 task_name=f'''wgs84 project {
                     os.path.basename(base_raster_path)}''')
+
+            mosaic_complete_token_path = f'''{
+                os.path.splitext(target_wgs84_raster_path)[0]}.MOSAICKED'''
+            mosiac_task = task_graph.add_task(
+                func=mosaic_base_into_target,
+                args=(
+                    target_wgs84_raster_path,
+                    global_raster_task_path_map[raster_suffix][1],
+                    mosaic_complete_token_path),
+                ignore_path_list=[
+                    global_raster_task_path_map[raster_suffix][1]],
+                target_path_list=[mosaic_complete_token_path],
+                dependent_task_list=(
+                    [wgs84_project_task]+previous_project_task_list),
+                task_name=f'''mosiac {
+                    os.path.basename(target_wgs84_raster_path)}''')
+            # this ensures that a mosiac will happen one at a time
+            previous_project_task_list = [mosiac_task]
             break
 
     task_graph.close()
     task_graph.join()
 
+
+def mosaic_base_into_target(
+        base_raster_path, target_raster_path, target_token_complete_path):
+    """Copy valid parts of base to target w/r/t correct georeference.
+
+    Parameters:
+        base_raster_path (str): a raster with the same cell size,
+            coordinate system, and nodata as `target_raster_path`.
+        target_raster_path (str): a raster that already exists on disk that
+            after this call will contain the non-nodata parts of
+            `base_raster_path` that geographically overlap with the target.
+        target_token_complete_path (str): this file is created if the
+            mosaic to target is successful. Useful for taskgraph task
+            scheduling.
+
+    Returns:
+        None.
+
+    """
+    target_raster = gdal.OpenEx(
+        target_raster_path, gdal.OF_RASTER | gdal.GA_Update)
+    target_band = target_raster.GetRasterBand(1)
+    target_raster_info = pygeoprocessing.get_raster_info(target_raster_path)
+    target_nodata = target_raster_info['nodata'][0]
+    base_raster_info = pygeoprocessing.get_raster_info(base_raster_path)
+    target_gt = target_raster_info['geotransform']
+    base_gt = base_raster_info['geotransform']
+
+    target_x_off = int((base_gt[0] - target_gt[0]) / target_gt[1])
+    target_y_off = int((base_gt[3] - target_gt[3]) / target_gt[5])
+
+    for offset_dict, band_data in pygeoprocessing.iterblocks(
+            (base_raster_path, 1)):
+        target_block = target_band.ReadAsArray(
+            xoff=offset_dict['xoff']+target_x_off,
+            yoff=offset_dict['yoff']+target_y_off,
+            win_xsize=offset_dict['win_xsize'],
+            win_ysize=offset_dict['win_ysize'])
+
+        valid_mask = ~numpy.isclose(target_block, target_nodata)
+        target_block[valid_mask] = band_data[valid_mask]
+        target_band.WriteArray(
+            target_block,
+            xoff=offset_dict['xoff']+target_x_off,
+            yoff=offset_dict['yoff']+target_y_off)
+    target_band.FlushCache()
+    target_band = None
+    target_raster = None
 
 
 def make_empty_wgs84_raster(
@@ -164,6 +232,9 @@ def make_empty_wgs84_raster(
         target_raster_path (str): this is the target raster that will cover
             [-180, 180), [90, -90) with cell size units with y direction being
             negative.
+        target_token_complete_path (str): this file is created if the
+            mosaic to target is successful. Useful for taskgraph task
+            scheduling.
 
     Returns:
         None.

feae9b7d3971ce44578c0103322af3d62696c063
Rich Sharp <richpsharp@gmail.com>
2019-06-04 16:56:20 -0700

    logging

--- reclass_lulc.py
+++ reclass_lulc.py
@@ -1,7 +1,21 @@
 """Reclass the ESA lulc."""
+import sys
+import logging
+
 from osgeo import gdal
 import natcap.invest.utils
 import pygeoprocessing
+
+# set a 1GB limit for the cache
+gdal.SetCacheMax(2**30)
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format=(
+        '%(asctime)s (%(relativeCreated)d) %(levelname)s %(name)s'
+        ' [%(funcName)s:%(lineno)d] %(message)s'),
+    stream=sys.stdout)
+LOGGER = logging.getLogger(__name__)
 
 
 def main():
@@ -13,9 +27,11 @@ def main():
 
     biophysical_table = natcap.invest.utils.build_lookup_from_csv(
         table_path, 'esa_id', to_lower=True, warn_if_missing=True)
+    LOGGER.debug(biophysical_table)
     value_map = dict([
         (lucode, int(biophysical_table[lucode]['nathab']))]
         for lucode in biophysical_table)
+    LOGGER.debug(value_map)
     pygeoprocessing.reclassify_raster(
         (lulc_raster_path, 1), value_map, lulc_types_raster_path, gdal.GDT_Byte,
         128, values_required=True)
